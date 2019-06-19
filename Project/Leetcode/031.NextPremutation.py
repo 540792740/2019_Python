862487316c341113766ac082e2d440a3e99fca5d
Peter <peterwangjiawei@gmail.com>
2019-06-18 20:31:33 -0400

    June/18/2019_Leetcode_31

--- Project/Leetcode/031.NextPremutation.py
+++ Project/Leetcode/031.NextPremutation.py
@@ -15,6 +15,7 @@ class Solution(object):
         while i > 0 and nums[i] <= nums[i - 1]:
             i -= 1
         self.reverse(nums, i, n - 1)
+
         if i > 0:
             for j in range(i, n):
                 if nums[j] > nums[i - 1]:
@@ -25,11 +26,45 @@ class Solution(object):
         """
         contains i and j.
         """
-        for k in range(i, (i + j) / 2 + 1):
+        for k in range(i, int((i + j) / 2 + 1)):
             self.swap(nums, k, i + j - k)
+            print(nums)
 
     def swap(self, nums, i, j):
         """
         contains i and j.
         """
         nums[i], nums[j] = nums[j], nums[i]
+
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        ls = len(nums)
+        n = ls - 1
+        # n is index
+        while n > 0 and nums[n - 1] > nums[n]:
+            n -= 1
+        self.reverse(nums, n, ls - 1)
+        print(nums)
+        for i in range(n, ls):
+            if nums[i] > nums[n - 1]:
+                self.swap(nums, n - 1, i)
+            break
+        print(nums)
+    def swap(self, nums, i, j):
+        nums[i], nums[j] = nums[j], nums[i]
+
+    def reverse(self, nums, i, j):
+        for k in range(i, int((i + j) / 2 + 1)):
+            self.swap(nums, k, i + j - k)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    # s.nextPermutation([1,5,8,4,7,6,5,3,1])
+    # s.nextPermutation([1,2,3])
+    s.nextPermutation([2,3,1])
