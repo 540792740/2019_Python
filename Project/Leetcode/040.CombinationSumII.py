677c4d8a9a2b754337738adac435e0fc69f3f6d0
Peter <peterwangjiawei@gmail.com>
2019-06-20 16:20:55 -0400

    June/18/2019_Leetcode_40

--- Project/Leetcode/040.CombinationSumII.py
+++ Project/Leetcode/040.CombinationSumII.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
@@ -14,14 +14,16 @@ class Solution(object):
             for i in range(start, ls):
                 if target < candidates[i]:
                     break
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
                 else:
-                    dfs(target - candidates[i], i, vlist + [candidates[i]])
-        dfs(target,0,[])
+                    dfs(target - candidates[i], i + 1, vlist + [candidates[i]])
+        dfs(target, 0, [])
         return res
-
 
 
 if __name__ == '__main__':
     S = Solution()
-    a = S.combinationSum([8,7,4,3],11)
+    a = S.combinationSum2([4,6,2,8,3,3,5],13)
+    a = S.combinationSum2([2,5,2,1,2], 5)
     print(a)
