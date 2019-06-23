f7d3879112cf3d1bcbcc82a61c6cf19a233d739e
Peter <peterwangjiawei@gmail.com>
2019-06-23 16:29:54 -0400

    June/18/2019_Leetcode_53

--- Project/Leetcode/053.MaximumSubarray.py
+++ Project/Leetcode/053.MaximumSubarray.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        ls = len(nums)
+        if ls < 1:
+            return 0
+        else:
+            num_next = nums[0]
+            max_num = nums[0]
+            for i in range(1, ls):
+                num_next = max(nums[i], num_next + nums[i])
+                max_num = max(max_num, num_next)
+        return max_num
+
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+    print(a)
