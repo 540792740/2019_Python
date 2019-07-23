59209c7c7edefad3e66c18f96c264c2094edf202
Peter <peterwangjiawei@gmail.com>
2019-07-23 16:58:48 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Array/189.RotateArray.py
+++ Project/Leetcode/Array/189.RotateArray.py
@@ -5,3 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        k %= len(nums)
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
+
+
+if __name__ == '__main__':
+    S= Solution()
+    a = S.rotate([1,2,3,4,5,6,7], 3)
+    print(a)
