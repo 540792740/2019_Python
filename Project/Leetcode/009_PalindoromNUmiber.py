11ae02271125497566a702f7bd567c69a1435624
Peter <peterwangjiawei@gmail.com>
2019-08-29 15:26:51 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/009_PalindoromNUmiber.py
+++ Project/Leetcode/009_PalindoromNUmiber.py
@@ -1,13 +1,14 @@
-class Solution (object):
-		def isPalindorome(self, x):
-				if x < 0:
-						return False
-				else:
-						txt = str(x)
-						return txt == txt[::-1]
-						
+class Solution(object):
+    def isPalindorome(self, x):
+        if x < 0:
+            return False
+        else:
+            txt = str(x)
+            return txt == txt[::-1]
+
+
 if __name__ == "__main__":
-		S = Solution()
-		a = S.isPalindorome(-41234321) 
-		print(a)
-		
+    S = Solution()
+    a = S.isPalindorome(-41234321)
+    print(a)
+
