46283ca9fbdd0852074a41aaaa43137c4f8b0f0a
Peter <peterwangjiawei@gmail.com>
2019-08-29 17:43:03 -0400

    June/18/2019_Leetcode_55

---
+++ Project/Leetcode/014.LongestCommonPrefix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return''
+        res = ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+if __name__ == "__main__":
+    S = Solution()
+    # a = S.longestCommonPrefix(["flower","flow","flight"])
+    a = S.longestCommonPrefix(["aa","a"])
+    print(a)
