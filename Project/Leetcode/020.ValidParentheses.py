5e2f4d728a7d59de9cb424a748ec814e6b63804d
Peter <peterwangjiawei@gmail.com>
2019-08-30 13:07:28 -0400

    June/18/2019_Leetcode_55

---
+++ Project/Leetcode/020.ValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s)/2 == 0 and len(s) != 0:
+            return False
+        else:
+            pars = [None]
+            parmap = {')':'(', '}':'{',']':'['}
+            for c in s:
+                if c in parmap and parmap[c] == pars[len(pars) - 1]:
+                    a = pars.pop()
+                else:
+                    pars.append(c)
+        return len(pars) == 1
+if __name__ == '__main__':
+    S = Solution()
+    a = S.isValid("([])")
+    print(a)
