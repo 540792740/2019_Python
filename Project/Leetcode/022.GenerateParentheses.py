e215a19e40dda3bf90908ca99bdac8f029ea3664
Peter <peterwangjiawei@gmail.com>
2019-08-30 13:27:00 -0400

    June/18/2019_Leetcode_55

---
+++ Project/Leetcode/022.GenerateParentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        self.generate(n,n,'',res)
+        return res
+
+    def generate(self, left, right, str, res):
+           if left == 0 and right == 0:
+               res.append(str)
+           if left > 0:
+               self.generate(left - 1, right, str+'(', res)
+           if left < right:
+               self.generate(left, right - 1, str+')', res)
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.generateParenthesis(4)
+    print(a)
