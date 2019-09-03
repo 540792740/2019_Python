3653fbbff73d422c29513e1dba36a1305211d0fe
Peter <peterwangjiawei@gmail.com>
2019-09-03 16:54:16 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/005.LongestPalindromicSubstring.py
+++ Project/Leetcode/005.LongestPalindromicSubstring.py
@@ -4,6 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        s_rev = s[::-1]
+        max, loc = (0,0)
+        length = len(s)
+        dp = []
+        for i in range(length + 1):
+            dp += [[0]*(length + 1)]
+        print(dp)
+        print()
+        for i in range (length):
+            for j in range(length):
+                if s[i] is s_rev[j]:
+                    Tmp = dp[i + 1][j + 1] = dp[i][j] + 1
+                    print(dp)
+                    print()
 
 
 if __name__ == "__main__":
