bcad7a1e67ca56188fd571d8f41f4cf122246c0b
Peter <peterwangjiawei@gmail.com>
2019-09-07 18:41:51 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/008.StringtoInteger(atoi).py
+++ Project/Leetcode/008.StringtoInteger(atoi).py
@@ -25,6 +25,24 @@ class Solution(object):
             number = -2147483648
         return number
 
+'''
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        str = str.strip()
+        try:
+            res = re.search('(^[\+\-]?\d+)', str).group()
+            res = int(res)
+            res = res if res <= 2147483647 else 2147483647
+            res = res if res >= -2147483648 else -2147483648
+        except:
+            res = 0
+        return res
+
+'''
 if __name__ == '__main__':
     S = Solution()
     a = S.myAtoi("-2147483649 with words")
