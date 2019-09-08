245da0db30bc68462816f5fb9d35fdc0baff6c8b
Peter <peterwangjiawei@gmail.com>
2019-09-08 16:44:19 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Dynamic Programming/000_BasicKnowledge.py
+++ Project/Leetcode/Dynamic Programming/000_BasicKnowledge.py
@@ -10,12 +10,21 @@ class basic(object):
     def LCS(self,s1, s2):
         length1 = len(s1)
         length2 = len(s2)
-        matrix = [[0]*(s1 + 1)]*(s2 + 1)
+        matrix = [[0]*(length1 + 1)]* (length2 + 1)
         print(matrix)
+        L_str = 0
+        last_number = 0
+        for i in range(length1):
+            for j in range(length2):
+                if s1[i] == s2[j]:
+                    matrix[i + 1][j + 1] = matrix[i][j] + 1
+                    if matrix[i + 1][j + 1] > L_str:
+                        L_str = matrix[i + 1][j + 1]
+                        last_number = i + 1
+        return s1[last_number - L_str:last_number]
 
-        return
 
 if __name__ == "__main__":
     S = basic()
-    S.LCS('abcj','jabcs')
-
+    a = S.LCS('abcj','jabcs')
+    print(a)
