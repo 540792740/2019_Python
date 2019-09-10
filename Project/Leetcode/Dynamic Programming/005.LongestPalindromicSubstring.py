8e7e22c517f148faed556ff2ed4acbc9dd20a5b8
Peter <peterwangjiawei@gmail.com>
2019-09-10 12:17:35 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Dynamic Programming/005.LongestPalindromicSubstring.py
+++ Project/Leetcode/Dynamic Programming/005.LongestPalindromicSubstring.py
@@ -4,51 +4,48 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s_rev = s[::-1]
-        maxlen = 0
-        dp = []
         length = len(s)
         if length <= 1:
             return s
-        for i in range(length + 1):
-            dp += [[0] * (length + 1)]
-        for i in range(length):
-            for j in range(length):
-                if s[i] is s_rev[j]:
-                    r = dp[i + 1][j + 1] = dp[i][j] + 1
-                    if r >= maxlen:
-                        maxlen = r
-                        end = i
-        print(end, maxlen)
-        print(dp)
-        return s[end - maxlen + 1 : end + 1]
+        if length == 2:
+            if s[0] == s[1]:
+                return s
+            return s[0]
+        max_length = 1
+        res = s[0]
+        i = 0
+        while i < length:
+            j = i + 1
+            '''make same charactor aabbbbaa, bbbb together'''
+            while j < length:
+                if s[i] == s[j]:
+                    j += 1
+                else:
+                    break
 
+            k = 0
+            print(i,j)
+            '''left - 1, right + 1, find longest length'''
+            while i - k - 1 >= 0 and j + k <= length - 1:
+                if s[i - k - 1] !=s[j + k]:
+                    break
+                k += 1
+
+            if j - i + 2 * k > max_length:
+                print('s',j - i + 2*k)
+                max_length = j - i + 2 * k
+                res = s[i - k : j + k]
+                # print(max_length,res)
+            if j + k == length - 1:
+                break
+            i = j
+        return res
 
 if __name__ == "__main__":
     S = Solution()
-    # a = S.longestPalindrome("bababd")
-    a = S.longestPalindrome("aacdecaa")
+    # a = S.longestPalindrome("cbababd")
+    a = S.longestPalindrome("aabbba")
     # a = S.longestPalindrome("bac")
     # a = S.longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     print(a)
 
-'''
-class Solution(object):
-def longestPalindrome(self, s):
-    """
-    :type s: str
-    :rtype: str
-    """
-    s_rev = s[::-1]
-    max, loc = (0, 0)
-    length = len(s)
-    D0 = []
-    for row in xrange(length+1): D0 += [[0]*(length+1)]
-    for i in range(length):
-        for j in range(length):
-            if s[i] is s_rev[j]:
-                Tmp = D0[i+1][j+1] = D0[i][j] + 1
-                if max <= Tmp:
-                    max, loc = Tmp, i
-    return s[loc-max+1:loc+1]
-'''
