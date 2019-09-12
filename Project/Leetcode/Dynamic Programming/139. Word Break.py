800add72389d2c7d0950e3ecf396137a43427eeb
Peter <peterwangjiawei@gmail.com>
2019-09-12 18:57:05 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Dynamic Programming/139. Word Break.py
+++ Project/Leetcode/Dynamic Programming/139. Word Break.py
@@ -5,3 +5,17 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
+        length = len(s)
+        dp = [True] + [False] * length
+
+        for i in range(length):
+            for j in range(0, i + 1):
+                if dp[j] and s[j:i+1] in wordDict:
+                    dp[i + 1] = True
+                    break
+        return dp[-1]
+if __name__ == '__main__':
+    S = Solution()
+    a = S.wordBreak("a", ["a"])
+    # a = S.wordBreak("catsandog", wordDict = ["cats", "og", "sand", "and", "cat"])
+    print(a)
