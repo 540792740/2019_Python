7e695512bd6f2e34847a0b8d2bdfca60d753cf1b
Peter <peterwangjiawei@gmail.com>
2019-09-13 15:41:04 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Dynamic Programming/120. Triangle.py
+++ Project/Leetcode/Dynamic Programming/120. Triangle.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type triangle: List[List[int]]
         :rtype: int
         """
-
+        length = len(triangle)
+        dp = triangle[-1]
+        for i in range(length - 2, -1, -1):
+            for j in range(0, i + 1):
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+        return dp[0]
 
 if __name__ == '__main__':
     S = Solution()
     a = S.minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]])
+    a = S.minimumTotal()
     print(a)
