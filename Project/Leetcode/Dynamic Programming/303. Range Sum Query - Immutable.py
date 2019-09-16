1309eda7ea4ac1ddeed8bcedd4d445b3ac489855
Peter <peterwangjiawei@gmail.com>
2019-09-16 19:28:58 -0400

    June/18/2019_Leetcode_55

--- Project/Leetcode/Dynamic Programming/303. Range Sum Query - Immutable.py
+++ Project/Leetcode/Dynamic Programming/303. Range Sum Query - Immutable.py
@@ -4,6 +4,16 @@ class NumArray(object):
         """
         :type nums: List[int]
         """
+        n = len(nums)
+        if n == 0:
+            return
+        self.dp = [0] * n
+        self.dp [0] = nums[0]
+        for i in range(1,n):
+            self.dp[i] = (nums[i] + self.dp[i - 1])
+        print(self.dp)
+
+
 
     def sumRange(self, i, j):
         """
@@ -11,7 +21,20 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
+        if i > 0:
+            return self.dp[j] - self.dp[i - 1]
+        else:
+            return self.dp[j]
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(i,j)
+
+
+if __name__ == '__main__':
+    # S = NumArray([-2,0,3,-5,2,-1])
+    S = NumArray([])
+    a = S.sumRange()
+    # a = S.sumRange(2,5)
+    # a = S.sumRange(0,5)
+    print(a)
