bcf47544651bd4459db62be1494d2c118be16a11
Wjw <peterwangjiawei@gmail.com>
2019-10-05 04:07:44 +0800

    Leetcode 108

--- Project/Leetcode/DFS/108.ConvertSortedArraytoBinarySearchTree.py
+++ Project/Leetcode/DFS/108.ConvertSortedArraytoBinarySearchTree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def sortedArrayToBST(self, nums):
@@ -11,3 +11,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
+        size = len(nums)
+        if size == 0:
+            return None
+        if size == 1:
+            return TreeNode(nums[0])
+        size //= 2
+        root = TreeNode(nums[size])
+        root.left = self.sortedArrayToBST(nums[:size])
+        root.right = self.sortedArrayToBST(nums[size+1:])
+        return root
