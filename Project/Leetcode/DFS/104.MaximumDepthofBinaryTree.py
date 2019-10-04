98f036c3b936dd34a231758b1ec9a5378ca09925
Wjw <peterwangjiawei@gmail.com>
2019-10-05 02:39:48 +0800

    Leetcode 104

---
+++ Project/Leetcode/DFS/104.MaximumDepthofBinaryTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.maxDepth([3,9,20,'','',15,7])
+    print(a)
