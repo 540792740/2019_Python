feab90276d666444befe098e0a1cb42c371e2169
Wjw <peterwangjiawei@gmail.com>
2019-10-05 05:56:51 +0800

    Leetcode 695

--- Project/Leetcode/DFS/695.MaxAreaofIsland.py
+++ Project/Leetcode/DFS/695.MaxAreaofIsland.py
@@ -1,25 +1,35 @@
+'''
+Time: O(m*n)
+Space: O(1)
+
+'''
+
 class Solution(object):
     def maxAreaOfIsland(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        def dfs(i,j):
-            if 0 <= i < row and 0 <= j < col and grid[i][j] == 1:
+        row = len(grid)
+        col = len(grid[0])
+        if row == 0 or col == 0:
+             return 0
+        res = 0
+
+        def dfs(i, j):
+            if 0 <= i < row and 0 <=j < col and grid[i][j] == 1:
                 grid[i][j] = 0
                 return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
             return 0
 
-        row, col = len(grid), len(grid[0])
-        ans = 0
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
-                    ans = max(ans, dfs(i, j))
+                    res =  max(res, dfs(i,j))
+        return res
 
-        return ans
 if __name__ == '__main__':
     S = Solution()
-    a = S.maxAreaOfIsland([[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]])
+    a = S.maxAreaOfIsland([[1,1,0,0,0],[1,1,1,0,0],[0,0,0,1,1],[0,0,0,1,1]])
     print(a)
 
