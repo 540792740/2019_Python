10cb88a522793ce9983b05ca5a18698d13f28d6a
Wjw <peterwangjiawei@gmail.com>
2019-10-05 08:04:48 +0800

    Leetcode 53

--- Project/Leetcode/Dynamic Programming/053.MaximumSubarray.py
+++ Project/Leetcode/Dynamic Programming/053.MaximumSubarray.py
@@ -1,3 +1,13 @@
+'''
+for this question
+0.  make sure return when nums length 0, 1
+1.  at first, maximum value is nums[0]
+2.  write loop from nums[1] to end, next value should or not add to previous
+    array. Compare the value with add new number to subarray or new subarray.
+
+'''
+
+
 class Solution(object):
     def maxSubArray(self, nums):
         ls = len(nums)
@@ -15,4 +25,5 @@ class Solution(object):
 if __name__ == '__main__':
     S = Solution()
     a = S.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+    a = S.maxSubArray([1,2,3,4,-1,-2,-3,7])
     print(a)
