39df7796129953b28e0199e75f1885bf2b0537e7
peter <peterwangjiawei@gmail.com>
2019-10-05 19:52:13 +0800

    Leetcode 105

--- Project/Leetcode/DFS/105.ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ Project/Leetcode/DFS/105.ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -1,3 +1,10 @@
+'''
+0.  No elment
+1.  First element of inoder array is the root,
+    second element is left part root.
+    Third element is right part root....
+2.  Rebuild Tree
+'''
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -12,3 +19,15 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
         """
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind + 1:])
+            return root
+
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.buildTree([3,9,20,15,7], [9,3,15,20,7])
+    print(a)
