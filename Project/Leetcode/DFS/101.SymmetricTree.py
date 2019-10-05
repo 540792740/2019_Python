e190cf0c64bc0865bd62401bacecaeb66f9f6bf1
peter <peterwangjiawei@gmail.com>
2019-10-05 20:59:15 +0800

    Leetcode 101

--- Project/Leetcode/DFS/101.SymmetricTree.py
+++ Project/Leetcode/DFS/101.SymmetricTree.py
@@ -11,4 +11,35 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        arr = []
+        if root:
+            arr.append(root)
+        while arr:
+            vals = []
+            for i in arr:
+                if i:
+                    vals.append(i.val)
+                else:
+                    vals.append(None)
+            if list(reversed(vals)) != vals:
+                return False
+            else:
+                temp = []
+                for i in arr:
+                    if i:
+                        temp.append(i.left)
+                        temp.append(i.right)
+                arr = temp
+        return True
 
+if __name__ == '__main__':
+    root = TreeNode(1)
+    left = root.left = TreeNode(2)
+    right = root.right = TreeNode(2)
+    left.left = TreeNode(3)
+    left.right = TreeNode(4)
+    right.left = TreeNode(4)
+    right.right = TreeNode(3)
+    S = Solution()
+    a = S.isSymmetric(root)
+    print(a)
