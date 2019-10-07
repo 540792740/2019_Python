13b50312c0fdc30e25cc2b78b33893585506111f
peter <peterwangjiawei@gmail.com>
2019-10-07 18:57:33 +0800

    Leetcode 199

--- Project/Leetcode/DFS/199.BinaryTreeRightSideView.py
+++ Project/Leetcode/DFS/199.BinaryTreeRightSideView.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def rightSideView(self, root):
@@ -11,3 +11,26 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        # Recursive
+        if not root:
+            return[]
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right):]
+
+    # DFS
+    def rightSideView1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def collect(node, depth):
+            if node:
+                if depth == len(view):
+                    view.append(node.val)
+                collect(node.right, depth + 1)
+                collect(node.left, depth + 1)
+
+        view =[]
+        collect(root,0)
+        return view
