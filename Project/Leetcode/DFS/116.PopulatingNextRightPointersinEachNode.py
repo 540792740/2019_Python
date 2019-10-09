c3c0e4b99f437c98f997e265544b0c1d68a3ee1b
peter <peterwangjiawei@gmail.com>
2019-10-09 19:12:30 +0800

    Leetcode 116

--- Project/Leetcode/DFS/116.PopulatingNextRightPointersinEachNode.py
+++ Project/Leetcode/DFS/116.PopulatingNextRightPointersinEachNode.py
@@ -1,5 +1,7 @@
 """
 # Definition for a Node.
+Key is left.next = right
+       when no next, move to cur.left
 """
 class Node(object):
     def __init__(self, val, left, right, next):
@@ -16,3 +18,16 @@ class Solution(object):
         :type root: Node
         :rtype: Node
         """
+        if not root:
+            return None
+        cur = root
+        next = root.left
+
+        while cur.left:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = next
+                next = cur.left
