7fb0d8b21204ed743dc381db9b8d1f9251dba86e
peter <peterwangjiawei@gmail.com>
2019-10-10 17:19:57 +0800

    Leetcode 257

--- Project/Leetcode/DFS/257.BinaryTreePaths.py
+++ Project/Leetcode/DFS/257.BinaryTreePaths.py
@@ -11,3 +11,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[str]
         """
+        if not root:
+            return []
+        res = []
+        stack = [(root,'')]
+        while stack:
+            node, ls = stack.pop()
+            if not node.left and not node.right:
+                res.append(ls + str(node.val))
+            if node.right:
+                stack.append((node.right, ls + str(node.val) + '->'))
+            if node.left:
+                stack.append((node.left, ls + str(node.val) + '->'))
+
+            return res
