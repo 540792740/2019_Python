31ed2142c8facecde8883ab62994756003b0be31
peter <peterwangjiawei@gmail.com>
2019-10-10 18:46:29 +0800

    Leetcode 111

---
+++ Project/Leetcode/DFS/111.MinimumDepthofBinaryTree.py
@@ -0,0 +1,24 @@
+'''
+0. root == None
+1. End Node return 1, None return 0
+2. Find minimize Depth
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        if root.left == None or root.right == None:
+            return self.minDepth(root.left) + self.minDepth(root.right) + 1
+        return min(self.minDepth(root.right), self.minDepth(root.left)) + 1
