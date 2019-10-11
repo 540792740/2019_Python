1a554763a167fdb0fb088d407b57942431e4cccf
peter <peterwangjiawei@gmail.com>
2019-10-11 15:48:05 +0800

    Leetcode 67

--- Project/Leetcode/String/067.AddBinary.py
+++ Project/Leetcode/String/067.AddBinary.py
@@ -5,3 +5,13 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        a = int(a,2) + int(b,2)
+        a = str(a)
+        a = bin(int(a, 10))
+        a = a[2:len(a)]
+        return a
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.addBinary("11", "11")
+    print(a)
