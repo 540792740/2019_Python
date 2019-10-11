c84cb2a2cbed6ff9a7199abcc012e6f286d63c09
peter <peterwangjiawei@gmail.com>
2019-10-11 17:51:58 +0800

    Leetcode 49

--- Project/Leetcode/String/049.GroupAnagrams.py
+++ Project/Leetcode/String/049.GroupAnagrams.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        d = {}
+        for w in sorted(strs):
+            key = tuple(sorted(w))
+            d[key] = d.get(key,[]) + [w]
+            print(d[key])
+        return d.values()
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+    print(a)
---
+++ Project/Leetcode/String/929.UniqueEmailAddresses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        def parse(email):
+            local, domain = email.split('@')
+            local = local.split('+')[0].replace('.','')
+            return f'{local}@{domain}'
+        return len(set(map(parse,emails)))
+
+if __name__ == '__main__':
+    S = Solution()
+    a = S.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"])
+    print(a)
