a0f9ec36fdfafe3b34ca22992c94e7aadf409dbe
peter <peterwangjiawei@gmail.com>
2019-10-12 15:08:09 +0800

    Leetcode 647 Two point, for Loop, DP

--- Project/Leetcode/String/647.PalindromicSubstrings.py
+++ Project/Leetcode/String/647.PalindromicSubstrings.py
@@ -38,8 +38,23 @@ class Solution(object):
                 if s[i:j] == s[i:j][::-1]:
                     res += 1
         return res
+
+    # DP 30%
+    # def countSubstrings2(self, s):
+    #     n = len(s)
+    #     count = 0
+    #     start, end, maxL = 0, 0, 0
+    #     dp = [[False] * n for i in range(n)]
+    #     for i in range(n):
+    #         for j in range(i):
+    #             dp[j][i] = (s[j] == s[i]) & ((i - j < 2) | dp[j + 1][i - 1])
+    #             if dp[j][i]:
+    #                 count += 1
+    #         dp[i][i] = 1
+    #         count += 1
+    #     return count
 if __name__ == '__main__':
     S = Solution()
     a = S.countSubstrings("aaa")
-    a = S.countSubstrings("aaaBBCC")
+    a = S.countSubstrings2("aaaBBCC")
     print(a)
