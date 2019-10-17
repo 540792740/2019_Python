e6d068d806b71471105cc3230001ec8f966b1371
peter <peterwangjiawei@gmail.com>
2019-10-17 20:30:54 +0800

    Leetcode tree 572

--- Project/Leetcode/Amazon/572. Subtree of Another Tree.py
+++ Project/Leetcode/Amazon/572. Subtree of Another Tree.py
@@ -1,9 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+'''
+DFS
+0.
+1.
+2.
+
+Time: O(m*n)
+Space: O(1)
+'''
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def isSubtree(self, s, t):
@@ -12,3 +21,21 @@ class Solution(object):
         :type t: TreeNode
         :rtype: bool
         """
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+
+        return self.isSameTree(s,t) or self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
+
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        if s.val != t.val:
+            return False
+        else:
+            return self.isSameTree(s.left,t.left) and self.isSameTree(s.right, t.right)
+
+
