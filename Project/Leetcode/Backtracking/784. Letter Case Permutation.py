6679a19d0bcf8a5f6f25a9865bc088ca401f04ba
peter <peterwangjiawei@gmail.com>
2019-10-19 15:31:05 +0800

    Leetcode Backtraking 22

--- Project/Leetcode/Backtracking/784. Letter Case Permutation.py
+++ Project/Leetcode/Backtracking/784. Letter Case Permutation.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type S: str
         :rtype: List[str]
         """
+        digits = {str(x) for x in range(0,10)}
+        print(digits)
+        A = ['']
+        for s_digit in S:
+            Alpha = []
+            if s_digit in digits:
+                for a_digit in A:
+                    Alpha.append(a_digit + s_digit)
+            else:
+                for a_digit in A:
+                    Alpha.append(a_digit+ s_digit.lower())
+                    Alpha.append(a_digit+ s_digit.upper())
+            A = Alpha
+        return A
+if __name__ == '__main__':
+    S = Solution()
+    test = S.letterCasePermutation("a1b2")
+    print(test)
