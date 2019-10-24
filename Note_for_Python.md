077364176c94ee96f61548dab62dc4fc2a622874
peter <peterwangjiawei@gmail.com>
2019-10-24 16:50:41 -0400

    Leetcode Tree 1022

--- Note_for_Python.md
+++ Note_for_Python.md
@@ -1,6 +1,5 @@
 ## I. Collections
 [link](http://www.google.com)
-
 <br>List * number： [2] * 2 =  [2,2]<br> 
 
 #### 1. Function `'enumerate'`
---
+++ Project/Leetcode/Tree/1022. Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,33 @@
+# 100% beat
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def sumRootToLeaf(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        self.res = []
+
+        def helper(root, path):
+            if not root.left and not root.right:
+                self.res.append(path)
+                return
+            if root:
+                if root.left:
+                    helper(root.left, path + str(root.left.val))
+                if root.right:
+                    helper(root.right, path + str(root.right.val))
+
+        helper(root, str(root.val))
+        summary = 0
+        for i in self.res:
+            summary += int(i, 2)
+        return summary
