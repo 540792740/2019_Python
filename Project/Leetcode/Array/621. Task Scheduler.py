720e46cb13ed848d732904d6bb1a4483fc1d40ed
peter <peterwangjiawei@gmail.com>
2019-10-28 18:10:38 -0400

    Leetcode array 621

--- Project/Leetcode/Array/621. Task Scheduler.py
+++ Project/Leetcode/Array/621. Task Scheduler.py
@@ -1,3 +1,7 @@
+'''
+max_val - 1 means last time, there no need to idel
+n + 1 means every iterate, there should have n + 1 numbers
+'''
 class Solution:
     def leastInterval(self, tasks, n):
         count = 0
@@ -10,11 +14,11 @@ class Solution:
                 dic[i] = 1
         max_val = max(dic.values())
         for i in dic.values():
-            print(i)
-        print(dic.values())
-
+            if i == max_val:
+                count += 1
+        return max((max_val - 1) * (n + 1) + count, ls)
 
 if __name__ == '__main__':
     S = Solution()
-    test = S.leastInterval(["A","A","A","B","B","B"], 2)
+    test = S.leastInterval(["B","B","B"], 2)
     print(test)
