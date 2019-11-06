2eca63a238c58de6fa2223767b5feab8de43954c
peter <peterwangjiawei@gmail.com>
2019-11-06 14:10:10 -0500

    Amazon

--- Project/Leetcode/Amazon/1192. Critical Connections in a Network.py
+++ Project/Leetcode/Amazon/1192. Critical Connections in a Network.py
@@ -2,7 +2,51 @@ from collections import defaultdict
 import collections
 
 class Solution(object):
+    # Tarjan 89%
     def criticalConnections(self, n, connections):
+        adj = defaultdict(set)
+        for i, j in connections:
+            adj[i].add(j)
+            adj[j].add(i)
+        print(adj)
+
+        # Init Visited, dfn, low
+        visited = [False] * n
+        dfn = [0] * n
+        low = [0] * n
+        self.time = 0
+        parent = [-1] * n
+
+        res = []
+
+        # Tarjan algorithm find strong connectivity
+        def tarjan(u):
+            visited[u] = True
+            self.time += 1
+            dfn[u] = low[u] = self.time
+            for v in adj[u]:
+                if not visited[v]:
+                    parent[v] = u
+                    tarjan(v)
+                    low[u] = min(low[u], low[v])
+
+                    if low[v] > dfn[u]:
+                        res.append([u,v])
+
+                # reboot low[u] as the lowest number to u
+                elif v != parent[u]:
+                    low[u] = min(low[u], dfn[v])
+
+            return
+
+        for root in range(n):
+            if not visited[root]:
+                tarjan(root)
+        return res
+
+
+    # 2 DFS
+    def criticalConnections1(self, n, connections):
         """
         :type n: int
         :type connections: List[List[int]]
@@ -43,5 +87,6 @@ class Solution(object):
 
 if __name__ == '__main__':
     S = Solution()
-    test = S.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3],[3,2]])
+    # test = S.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3],[3,2]])
+    test = S.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3]])
     print(test)
