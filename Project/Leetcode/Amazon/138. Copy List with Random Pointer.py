4e5e38f07540d50848b7f5738e56d5235a799628
peter <peterwangjiawei@gmail.com>
2019-11-07 15:23:30 -0500

    Amazon

--- Project/Leetcode/Amazon/138. Copy List with Random Pointer.py
+++ Project/Leetcode/Amazon/138. Copy List with Random Pointer.py
@@ -7,61 +7,37 @@ class Node(object):
         self.next = next
         self.random = random
 class Solution(object):
-
-    # 99%
     def copyRandomList(self, head):
-        if head is None:
-            return None
-        mapping = {}
-        cur = head
-        while cur:
-            mapping[cur] = Node(cur.val, None, None)
-            cur = cur.next
-        cur = head
-        while cur:
-            if cur.next:
-                mapping[cur].next = mapping[cur.next]
-            if cur.random:
-                mapping[cur].random = mapping[cur.random]
-            cur = cur.next
-        return mapping[head]
-
-
-    def copyRandomList1(self, head):
         """
         :type head: Node
         :rtype: Node
         """
         return copy.deepcopy(head)
 
-
-    def copyRandomList2(self, head: 'Node') -> 'Node':
+    def copyRandomList1(self, head: 'Node') -> 'Node':
         # when head is None
         if not head: return None
 
+        # Hashtable, save all node into hashtable
         mapping = {}
-        cur = head
+        temp = head
+        while temp:
+            mapping[temp] = Node(temp.val, None, None)
+            temp = temp.next
 
-        # Save all Node into dic-mapping
-        while cur:
-            mapping[cur] = Node(cur.val, None, None)
-            cur = cur.next
-        cur = head
+        temp = head
+        while temp:
 
-        # Adjest Pointer and Copy
-        while cur:
+            # Adjust pointer 'next'
+            if temp.next:
+                mapping[temp].next = mapping[temp.next]
 
-            # 'next' pointer copy
-            if cur.next:
-                mapping[cur].next = mapping[cur.next]
-
-            # 'random' pointer copy
-            if cur.random:
-                mapping[cur].random = mapping[cur.random]
+            # Adjust pointer 'random'
+            if temp.random:
+                mapping[temp].random = mapping[temp.random]
 
             # Move to next Node
-            cur = cur.next
+            temp = temp.next
 
         return mapping[head]
-
 
