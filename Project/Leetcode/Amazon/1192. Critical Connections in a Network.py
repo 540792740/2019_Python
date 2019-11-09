d881984433ffa1e9613f0bc4b70cf763668b0be5
peter <peterwangjiawei@gmail.com>
2019-11-08 21:21:14 -0500

    Amazon

--- Project/Leetcode/Amazon/1192. Critical Connections in a Network.py
+++ Project/Leetcode/Amazon/1192. Critical Connections in a Network.py
@@ -4,46 +4,53 @@ import collections
 class Solution(object):
     # Tarjan 89%
     def criticalConnections(self, n, connections):
+
+        n = n + 1
+        # Save all connections in to a dictionary(set)
         g = defaultdict(set)
         for i, j in connections:
             g[i].add(j)
             g[j].add(i)
+        print(g)
 
-        # Init Visited, dfn, low
+        # Init res, visited, low, dfn
         res = []
+        visited = [False] * n
         low = [0] * n
         dfn = [0] * n
         parent = [-1] * n
-        visited = [False] * n
         self.times = 0
 
-        # Tarjan algorithm find strong connectivity
+        # Init tarjan algorithm, find strong connectivity
         def tarjan(root):
+            print(low, dfn)
             self.times += 1
             low[root] = dfn[root] = self.times
             visited[root] = True
 
+            # Traverse all node connect with root
             for v in g[root]:
-                if not visited[v]:
+
+                # Not visited Node
+                if visited[v] == False:
                     parent[v] = root
                     tarjan(v)
                     low[root] = min(low[root], low[v])
 
+                    # This will be a strong connection if minpath is less than original.
                     if low[v] > dfn[root]:
+                        print(low, dfn, v, root)
                         res.append([v, root])
 
-                # reboot low[u] as the lowest number to u
+                # If v is not comes from root
                 elif v != parent[root]:
-                    low[root] = min(low[v], low[root])
+                    low[root] = min(low[root], low[v])
+
             return
 
-        # Traverse Node
         for root in range(n):
-            if not visited[root]:
-                tarjan(root)
-
+            if visited[root] == False: tarjan(root)
         return res
-
     # 2 DFS
     def criticalConnections1(self, n, connections):
         """
@@ -51,7 +58,6 @@ class Solution(object):
         :type connections: List[List[int]]
         :rtype: List[List[int]]
         """
-
         g = collections.defaultdict(list)
 
         # Save all path in to dic g
@@ -87,5 +93,9 @@ class Solution(object):
 if __name__ == '__main__':
     S = Solution()
     # test = S.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3],[3,2]])
-    test = S.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3]])
+    test = S.criticalConnections(3, [[0,1],[1,2],[2,0],[1,3]])
+    print(test)
+    test = S.criticalConnections(9, [[1, 2], [1, 3], [2, 3], [3, 4], [3, 6], [4, 5], [6, 7], [6, 9], [7, 8], [8, 9]])
+    print(test)
+    test = S.criticalConnections(6, [[1, 2], [1, 3], [2, 3], [2, 4], [2, 5], [4, 6], [5, 6]])
     print(test)
--- Project/Leetcode/Amazon/Critical Routers.py
+++ Project/Leetcode/Amazon/Critical Routers.py
@@ -1,36 +1,83 @@
 import collections
-
-
-class Router:
-    def __init__(self, val):
-        self.val = val
-        self.link = []
-    def addLink(self, router):
-        self.links.append(router)
+from collections import defaultdict
 
 class Solution:
-    def findCriticalRouters(self, numRouters, numLinks, Links):
-        if not numRouters: return []
-        if numRouters == 1: return[0]
+    def findCriticalRouters(self, n, connections):
+        g = defaultdict(set)
+        for i, j in connections:
+            g[i].add(j)
+            g[j].add(i)
 
-        routers = [Router(i) for i in range(numRouters)]
+        # Init Visited, dfn, low
         res = []
+        low = [0] * n
+        dfn = [0] * n
+        parent = [-1] * n
+        visited = [False] * n
+        self.times = 0
 
-        # for i, u in Links:
-        #     # routers[i]
-        #     print(i, u)
-        #     print(routers)
-        for link in Links:
-            routers[link[0]].links.append(link[1])
-            routers[link[1]].links.append(link[0])
+        # Tarjan algorithm find strong connectivity
+        def tarjan(root):
+            self.times += 1
+            low[root] = dfn[root] = self.times
+            visited[root] = True
 
-        return
+            for v in g[root]:
+                if not visited[v]:
+                    parent[v] = root
+                    tarjan(v)
+                    low[root] = min(low[root], low[v])
 
-    def dfs(self, rootNode, removedOne):
+                    if low[v] > dfn[root]:
+                        res.append([v, root])
 
-        return
+                # reboot low[u] as the lowest number to u
+                elif v != parent[root]:
+                    low[root] = min(low[v], low[root])
+            return
 
+        # Traverse Node
+        for root in range(n):
+            if not visited[root]:
+                tarjan(root)
+
+        return res
 if __name__ == '__main__':
     S = Solution()
-    test = S.findCriticalRouters(7, 7, [[0,1], [0, 2], [1, 3], [2, 3], [2, 5], [5, 6], [3,4]])
+    test = S.findCriticalRouters(7, [[0,1], [0, 2], [1, 3], [2, 3], [2, 5], [5, 6], [3,4]])
+    print(test, 'Result should be ', [[6, 5], [5, 2], [4, 3]])
+    test = S.findCriticalRouters(10, [[1, 2], [1, 3], [2, 3], [3, 4], [3, 6], [4, 5], [6, 7], [6, 9], [7, 8], [8, 9]])
     print(test)
+
+'''
+Given an underected connected graph with n nodes labeled 1..n. A bridge (cut edge) is defined as an edge which, when removed, makes the graph disconnected (or more precisely, increases the number of connected components in the graph). Equivalently, an edge is a bridge if and only if it is not contained in any cycle. The task is to find all bridges in the given graph. Output an empty list if there are no bridges.
+
+Input:
+
+n, an int representing the total number of nodes.
+edges, a list of pairs of integers representing the nodes connected by an edge.
+Example 1:
+
+Input: n = 5, edges = [[1, 2], [1, 3], [3, 4], [1, 4], [4, 5]]
+
+Output: [[1, 2], [4, 5]]
+Explanation:
+There are 2 bridges:
+1. Between node 1 and 2
+2. Between node 4 and 5
+If we remove these edges, then the graph will be disconnected.
+If we remove any of the remaining edges, the graph will remain connected.
+Example 2:
+
+Input: n = 6, edges = [，；[1, 2], [1, 3], [2, 3], [2, 4], [2, 5], [4, 6], [5, 6]]
+
+Output: []
+Explanation:
+We can remove any edge, the graph will remain connected.
+Example 3:
+
+Input: n = 9, edges = [[1, 2], [1, 3], [2, 3], [3, 4], [3, 6], [4, 5], [6, 7], [6, 9], [7, 8], [8, 9]]
+
+Output: [[3, 4], [3, 6], [4, 5]]
+'''
+
