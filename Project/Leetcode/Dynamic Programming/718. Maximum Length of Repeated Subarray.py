71b74a6756982cfc6f13bbd99a43755c2d4f2d4d
peter <peterwangjiawei@gmail.com>
2019-11-12 10:42:02 -0500

    Leetcode 1181, 718, 209

---
+++ Project/Leetcode/Dynamic Programming/718. Maximum Length of Repeated Subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        # Init matrix
+        matrix = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    matrix[i][j] = matrix[i -1][j - 1] + 1
+
+        return max(max(row) for row in matrix)
