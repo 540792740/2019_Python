4b620a36eab8d173b6885dc185ec3921905037bb
peter <peterwangjiawei@gmail.com>
2019-11-13 19:24:39 -0500

    Leetcode 852, 957, 103, 1167

--- Project/Leetcode/Clutter/1181. Before and After Puzzle.py
+++ Project/Leetcode/Clutter/1181. Before and After Puzzle.py
@@ -1,61 +1,4 @@
 class Solution(object):
-
-    def beforeAndAfterPuzzles(self, phrases):
-        """
-        :type phrases: List[str]
-        :rtype: List[str]
-        """
-        # Using two dicts and two lists:
-        # 1. Save first word, dic_first
-        # 3. Save result index and res as res_index, res
-        dic_first, res, res_index = {}, [], []
-        phrases = sorted(phrases)
-
-        # Save all first and last words into dict, at the same time find matched sentences
-        for i in range(len(phrases)):
-            phrases[i] = phrases[i].split()
-            if phrases[i]:
-                dic_first[phrases[i][0]] = dic_first.get(phrases[i][0], []) + [i]
-
-
-        for i, sentence in enumerate(phrases):
-            if sentence[-1] in dic_first:
-                res_index.append([i, dic_first[sentence[-1]]])
-
-        # Init function to generate res based on res_index
-        def generate_res(res_index, res):
-            for r in res_index:
-                for r_index in r[1]:
-                    if r_index != r[0]:
-                        temp1 = phrases[r[0]][:len(phrases[r[0]]) - 1]
-                        print(temp1, phrases[r[0]][:-1])
-                        temp2 = phrases[r_index]
-                        temp = ' '.join(temp1 + temp2)
-                        if temp not in res:
-                            res.append(temp)
-
-        generate_res(res_index, res)
-        return sorted(res)
-
-if __name__ == '__main__':
-    S = Solution()
-    test = S.beforeAndAfterPuzzles(["mission statement","a quick bite to eat","a chip off the old block", "chocolate bar","mission impossible", "a man on a mission","block party","eat my words","bar of soap a"])
-    test = S.beforeAndAfterPuzzles(["ghanuo qxi b vxbjg stdsoe xylid e jss skzkhfy f dihh id","vxbjg","wy uia bbg ulfoyo wmss e uia qznglzi kuoqsjm jjarip sdfrvleh agq qznglzi dihh skfm","wmss b chyfiw z chyfiw uvq qjrfodti pgjuaolq id aee ncbd cjmnmexu zclfup gwgeyw qxi aee id skzkhfy","wmss uia xylid bbg jjarip agq chyfiw skzkhfy uhmc aee ddicv e sdfrvleh nq","jss wj f agq uhmc pkore vxbjg ghanuo ddicv","qjrfodti e jss uhmc agq qznglzi nq id nq z aee vnckr skzkhfy pgjuaolq zclfup ulfoyo","pgjuaolq rwlxefgf gwgeyw kuoqsjm wj wmss b e","e aekq wmss agq uhmc vlyp wj qxi jss uhmc uvq ncbd vn wj agq","skfm jjarip wy agq pxqqtzw vn skfm z yoskkg zclfup e qjrfodti vlyp jjarip wj e b","sdfrvleh qznglzi uhmc id aee qznglzi z agq stdsoe","kuoqsjm cfjo skzkhfy rwlxefgf aekq chyfiw sdfrvleh nq e zclfup vlyp","qxi","agq kuoqsjm z skfm z jjarip aekq qznglzi skfm jss pxqqtzw ddicv skzkhfy e cfjo skzkhfy wy e","vxbjg qxi jjarip vn","id z jjarip jjarip yoskkg ncbd uia uia cjmnmexu","rwlxefgf cfjo pxqqtzw f chyfiw ddicv gwgeyw qxi vlyp jjarip pkore uvq f skzkhfy","wy uhmc vxbjg agq ghanuo zclfup z aee","bbg uia rwlxefgf skfm z z","z uvq zclfup wj agq stdsoe vnckr vlyp jjarip z ncbd b vnckr vlyp dihh f dihh vlyp agq pxqqtzw f","aekq z qznglzi chyfiw uia aee zclfup zclfup stdsoe z aee gwgeyw uhmc uyotjsgg thifuxm f","qznglzi cjmnmexu wy pkore vnckr wj wj wj","xylid z wmss e cfjo ulfoyo wy vn bbg ncbd yoskkg ghanuo b qznglzi ncbd xylid","gwgeyw pxqqtzw vnckr dihh sdfrvleh f z","e jjarip aekq bbg aekq wj z ghanuo pkore pgjuaolq","vn chyfiw uhmc id uvq","f vnckr z rwlxefgf f skzkhfy ncbd qznglzi thifuxm qjrfodti stdsoe uyotjsgg z id","kuoqsjm qznglzi wj yoskkg cjmnmexu gwgeyw gwgeyw thifuxm","gwgeyw f e ncbd aee xylid ddicv vlyp yoskkg ulfoyo z yoskkg chyfiw b qxi qjrfodti bbg","z vnckr sdfrvleh aekq uyotjsgg e vxbjg wy ulfoyo aekq skzkhfy wj f rwlxefgf pxqqtzw","e jjarip vnckr skfm rwlxefgf qjrfodti sdfrvleh aee wmss f","aee qznglzi qjrfodti uyotjsgg e z b vn uia cfjo ghanuo f thifuxm cjmnmexu aee z uhmc thifuxm pkore","wy uhmc sdfrvleh aekq zclfup kuoqsjm kuoqsjm z aee","qjrfodti z jss skzkhfy wj wy","bbg pgjuaolq zclfup wy","vxbjg vn wy","z e pxqqtzw vnckr pxqqtzw pkore b wmss gwgeyw uia ghanuo z cjmnmexu wj wj chyfiw jss","thifuxm vn kuoqsjm z vnckr wj yoskkg ncbd uvq kuoqsjm skzkhfy jss yoskkg chyfiw vlyp skfm dihh wy b","uvq uia qjrfodti cjmnmexu dihh vlyp jjarip qznglzi wmss wj","uvq z vn vn stdsoe uyotjsgg qxi z","uhmc aee stdsoe","f cfjo ddicv bbg skfm kuoqsjm qjrfodti e","chyfiw id f uhmc ddicv e qxi zclfup z vn e","id pkore e dihh uhmc stdsoe wj vn","aee skzkhfy jjarip uyotjsgg z aekq e wj pkore uvq wy dihh jss f","wy qznglzi pkore qjrfodti vlyp agq qxi","cjmnmexu dihh sdfrvleh stdsoe vnckr vxbjg uvq cjmnmexu uhmc agq xylid b","e z cjmnmexu uhmc qxi ddicv jss cjmnmexu vnckr e jjarip jjarip aekq ncbd e dihh","wmss vn qznglzi id skfm ncbd aee ghanuo zclfup wj pxqqtzw ulfoyo id","rwlxefgf stdsoe cjmnmexu f qxi ghanuo e uvq agq thifuxm stdsoe jss ulfoyo vlyp b ddicv e z"])
-    print(test)
-
-'''
-['a chip off the old block', 
-'a man on a mission', 
-'a quick bite to eat', 
-'bar of soap a', 
-'block party', 
-'chocolate bar', 
-'eat my words', 
-'mission impossible', 
-'mission statement']
-'''
-
-class Solution(object):
     def beforeAndAfterPuzzles(self, phrases):
         """
         :type phrases: List[str]
@@ -65,7 +8,6 @@ class Solution(object):
         # 1. Save first word into dic_first
         # 2. Save result's index into res_index
         # 3. Save restult into res[]
-
         dic_first, res, res_index = {}, [], []  # Init dic_first, res
 
         phrases = sorted(phrases)  # Sort phrases
@@ -77,7 +19,7 @@ class Solution(object):
             # Save all first word into dic, if there is duplicate word, save as a list of each index
             dic_first[phrases[i][0]] = dic_first.get(phrases[i][0], []) + [i]
 
-        # Find all possible res when last word in dic_first
+        # Find all possible result when last word in dic_first, save index into res_index
         for i, sentence in enumerate(phrases):
             if sentence[-1] in dic_first:
                 res_index.append([i, dic_first[sentence[-1]]])
@@ -96,6 +38,9 @@ class Solution(object):
         generate_res(res_index, res)
         return sorted(res)
 
-
-
+if __name__ == '__main__':
+    S = Solution()
+    test = S.beforeAndAfterPuzzles(["mission statement","a quick bite to eat","a chip off the old block", "chocolate bar","mission impossible", "a man on a mission","block party","eat my words","bar of soap a"])
+    test = S.beforeAndAfterPuzzles(["ghanuo qxi b vxbjg stdsoe xylid e jss skzkhfy f dihh id","vxbjg","wy uia bbg ulfoyo wmss e uia qznglzi kuoqsjm jjarip sdfrvleh agq qznglzi dihh skfm","wmss b chyfiw z chyfiw uvq qjrfodti pgjuaolq id aee ncbd cjmnmexu zclfup gwgeyw qxi aee id skzkhfy","wmss uia xylid bbg jjarip agq chyfiw skzkhfy uhmc aee ddicv e sdfrvleh nq","jss wj f agq uhmc pkore vxbjg ghanuo ddicv","qjrfodti e jss uhmc agq qznglzi nq id nq z aee vnckr skzkhfy pgjuaolq zclfup ulfoyo","pgjuaolq rwlxefgf gwgeyw kuoqsjm wj wmss b e","e aekq wmss agq uhmc vlyp wj qxi jss uhmc uvq ncbd vn wj agq","skfm jjarip wy agq pxqqtzw vn skfm z yoskkg zclfup e qjrfodti vlyp jjarip wj e b","sdfrvleh qznglzi uhmc id aee qznglzi z agq stdsoe","kuoqsjm cfjo skzkhfy rwlxefgf aekq chyfiw sdfrvleh nq e zclfup vlyp","qxi","agq kuoqsjm z skfm z jjarip aekq qznglzi skfm jss pxqqtzw ddicv skzkhfy e cfjo skzkhfy wy e","vxbjg qxi jjarip vn","id z jjarip jjarip yoskkg ncbd uia uia cjmnmexu","rwlxefgf cfjo pxqqtzw f chyfiw ddicv gwgeyw qxi vlyp jjarip pkore uvq f skzkhfy","wy uhmc vxbjg agq ghanuo zclfup z aee","bbg uia rwlxefgf skfm z z","z uvq zclfup wj agq stdsoe vnckr vlyp jjarip z ncbd b vnckr vlyp dihh f dihh vlyp agq pxqqtzw f","aekq z qznglzi chyfiw uia aee zclfup zclfup stdsoe z aee gwgeyw uhmc uyotjsgg thifuxm f","qznglzi cjmnmexu wy pkore vnckr wj wj wj","xylid z wmss e cfjo ulfoyo wy vn bbg ncbd yoskkg ghanuo b qznglzi ncbd xylid","gwgeyw pxqqtzw vnckr dihh sdfrvleh f z","e jjarip aekq bbg aekq wj z ghanuo pkore pgjuaolq","vn chyfiw uhmc id uvq","f vnckr z rwlxefgf f skzkhfy ncbd qznglzi thifuxm qjrfodti stdsoe uyotjsgg z id","kuoqsjm qznglzi wj yoskkg cjmnmexu gwgeyw gwgeyw thifuxm","gwgeyw f e ncbd aee xylid ddicv vlyp yoskkg ulfoyo z yoskkg chyfiw b qxi qjrfodti bbg","z vnckr sdfrvleh aekq uyotjsgg e vxbjg wy ulfoyo aekq skzkhfy wj f rwlxefgf pxqqtzw","e jjarip vnckr skfm rwlxefgf qjrfodti sdfrvleh aee wmss f","aee qznglzi qjrfodti uyotjsgg e z b vn uia cfjo ghanuo f thifuxm cjmnmexu aee z uhmc thifuxm pkore","wy uhmc sdfrvleh aekq zclfup kuoqsjm kuoqsjm z aee","qjrfodti z jss skzkhfy wj wy","bbg pgjuaolq zclfup wy","vxbjg vn wy","z e pxqqtzw vnckr pxqqtzw pkore b wmss gwgeyw uia ghanuo z cjmnmexu wj wj chyfiw jss","thifuxm vn kuoqsjm z vnckr wj yoskkg ncbd uvq kuoqsjm skzkhfy jss yoskkg chyfiw vlyp skfm dihh wy b","uvq uia qjrfodti cjmnmexu dihh vlyp jjarip qznglzi wmss wj","uvq z vn vn stdsoe uyotjsgg qxi z","uhmc aee stdsoe","f cfjo ddicv bbg skfm kuoqsjm qjrfodti e","chyfiw id f uhmc ddicv e qxi zclfup z vn e","id pkore e dihh uhmc stdsoe wj vn","aee skzkhfy jjarip uyotjsgg z aekq e wj pkore uvq wy dihh jss f","wy qznglzi pkore qjrfodti vlyp agq qxi","cjmnmexu dihh sdfrvleh stdsoe vnckr vxbjg uvq cjmnmexu uhmc agq xylid b","e z cjmnmexu uhmc qxi ddicv jss cjmnmexu vnckr e jjarip jjarip aekq ncbd e dihh","wmss vn qznglzi id skfm ncbd aee ghanuo zclfup wj pxqqtzw ulfoyo id","rwlxefgf stdsoe cjmnmexu f qxi ghanuo e uvq agq thifuxm stdsoe jss ulfoyo vlyp b ddicv e z"])
+    print(test)
 
--- Project/Leetcode/Clutter/Checking Your Route.py
+++ Project/Leetcode/Clutter/Checking Your Route.py
@@ -2,51 +2,67 @@
 class Solution(object):
 
     def minimumCost(self, start, end, N, connections):
+        # Using DP to solve the problem
+        # Init a matrix, save all connection into matrix
         matrix = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
         for i, j, cost in connections:
             matrix[i][j] = cost
             matrix[j][i] = cost
         self.min_cost = 2**31 - 1
-        self.res = []
+        self.min_cost_path = []
+
+        # Function dfs, find min cost path
         def dfs(start, path, cur_cost):
-            # print(path, cur_cost)
+
+            # Break when path cost more than minimum cost
             if cur_cost > self.min_cost:
                 return
+
+            # Renew when there is a more minimum cost
             elif start == end and self.min_cost > cur_cost:
-                self.res = []
-                self.res.append(path)
+                self.min_cost_path = []
+                self.min_cost_path.append(path)
                 self.min_cost = cur_cost
+
+            # Add minimum path
             elif start == end and self.min_cost == cur_cost:
-                self.res.append(path)
+                self.min_cost_path.append(path)
+
+            # Traverse from start to end, recursive
             for row in range(1, N + 1):
                 if matrix[start][row] > 0:
                     if row in path:
                         continue
                     dfs(row, path + [row], cur_cost + matrix[start][row])
         dfs(start, [start], 0)
+        print(self.min_cost_path)
 
-        def find_res(res):
+        #Based on the path of minimum cost, get the result
+        def find_res(min_cost_path):
             point_path = []
-            res_value = []
-            for i in res:
+            res = []
+
+            # Find all traversed node with minimum cost
+            for i in min_cost_path:
                 for j in range(1, len(i)):
-                    print(i)
                     point_path.append([i[j], i[j - 1]])
                     point_path.append([i[j - 1], i[j]])
-            print(point_path)
             for i, j, cost in connections:
-                print([i, j])
                 if [i, j] in point_path or [j, i] in point_path:
-                    res_value.append('YES')
+                    res.append('YES')
                 else:
-                    res_value.append('NO')
-            return res_value
-        return find_res(self.res)
+                    res.append('NO')
+            return res
+
+        return find_res(self.min_cost_path)
 
 if __name__ == '__main__':
     S = Solution()
     test = S.minimumCost(1, 5, 5, [[1,2,1],[2,3,1],[3,4,1],[4,5,1],[5,1,3],[1,3,2],[5,3,1]])
+    print(test)
     test = S.minimumCost(1, 4, 4, [[1,2,1],[2,4,1],[1,3,1],[3,4,2],[1,4,2]])
+    print(test)
     test = S.minimumCost(1, 5, 5, [[1,2,1],[2,3,1],[3,5,1],[1,4,1],[4,5,2],[3,4,2],[2,4,4]])
+    print(test)
     test = S.minimumCost(1, 4, 4, [[1,2,1],[1,3,1],[1,4,1],[2,3,1],[2,4,1]])
     print(test)
