6359897512b9d235c6367f447daecf5703036beb
peter <peterwangjiawei@gmail.com>
2019-12-23 21:39:30 -0500

    76

--- Project/Leetcode/Twopoint/76. Minimum Window Substring.py
+++ Project/Leetcode/Twopoint/76. Minimum Window Substring.py
@@ -5,3 +5,25 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        res = ''
+        l, count, minLen = 0, 0, 2 ** 31
+        dic = {}
+        for i in t: dic[i] = dic.get(i, 0) + 1
+        for i, c in enumerate(s):
+            dic[c] = dic.get(c, 0) - 1
+            if dic[c] >= 0:
+                count += 1
+            while count == len(t):
+                if minLen > i - l + 1:
+                    minLen = i - l + 1
+                    res = s[l : i + 1]
+                dic[s[l]]  = dic.get(s[l], 0) + 1
+                if dic[s[l]] > 0:
+                    count -= 1
+                l += 1
+        return res
+
+if __name__ == '__main__':
+    S = Solution()
+    test = S.minWindow("ADOBECODEBANC", "ABC")
+    print(test)
