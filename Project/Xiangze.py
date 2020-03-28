e7647c8b36006c26d20a2a5b1ce624a0b1875c3e
peter <peterwangjiawei@gmail.com>
2020-03-27 21:45:35 -0400

    mAR_27 pA

--- Project/Xiangze.py
+++ Project/Xiangze.py
@@ -1,13 +1,35 @@
 import pandas as pd
+# from nltk.stem.porter import PorterStemmer
+# porter_stemmer = PorterStemmer()
+# porter_stemmer.stem('wolves')
 
-data = pd.read_excel('test.xlsx')
-list1 = data.values.tolist()
-# 105 114
-ls = len(list1)
-Positive = []
-for i in range(ls):
-    print(list1[i][105])
-    if list1[i][105] == 1:
-        print(list1[i][0])
-        Positive.append(list1[i][0])
-print(Positive)
+# a ab abc.
+# a ab positive
+# abc nag
+
+
+
+# data = pd.read_excel('test.xlsx')
+# data = pd.read_excel('NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx')
+# list1 = data.values.tolist()
+a = str(True)
+print(a.lower())
+
+# print(type(True) == string)
+# print(list1[13086][0])
+# sentence = ["a","ab", "abc"]
+# p_count = 0
+# n_count = 0
+# for word in sentence:
+#     if word in Positive:
+#         p_count += 1
+#     if word in Nag:
+#         n_count += 1
+
+
+# for i in range(ls):
+#     print(list1[i][105])
+#     if list1[i][105] == 1:
+#         print(list1[i][0])
+#         Positive.append(list1[i][0].lower())
+# print(Positive)
